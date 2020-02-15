66d526e90bd2091828fa4e1a7cfe1c25cdc75c80
floorin <florin.codreanu@gmail.com>
2020-02-15 22:09:08 +0200

    test useEffect

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './about.css';
 import styles from './WillBeImportedAsStyle.css';
 
 function About() {
+    const [mesaj, setMesaj] = useState('');
+    const [count, setCount] = useState(0);
+    // Similar to componentDidMount and componentDidUpdate:
+    useEffect(() => {
+        // Update the document title using the browser API
+        setMesaj(`Ai dat click de ${count} ori!`);
+    });
+
     return <>
         <h2 className="about__text--big">Hi, I'm the "About" component!</h2>
-        <h5 className={styles.about}>Nothing special here</h5>
+        <h5 className={styles.about}>{mesaj}</h5>
+        <button onClick={() => setCount(count + 1)}>Click me!</button>
     </>;
 }
 export default About;
